e5dace142ebcbbe40ab32043af7df14bd2066815
LegendeJJ <dalienjj@gmail.com>
2022-06-13 15:33:54 +0200

    Added Basic style

--- seeds/index.js
+++ seeds/index.js
@@ -1,31 +1,35 @@
-const mongoose= require("mongoose");
-const cities= require("./cities")
-const {places, descriptors}= require("./seedHelpers")
+const mongoose = require("mongoose");
+const cities = require("./cities")
+const { places, descriptors } = require("./seedHelpers")
 const campground = require("../models/campground");
-const Campground=require("../models/campground")
+const Campground = require("../models/campground")
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
-    useNewUrlParser: true, 
+    useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error"));
-db.once("open", ()=>{
+db.once("open", () => {
     console.log("Database connected");
 });
 
-const sample= array => array[Math.floor(Math.random()*array.length)];
+const sample = array => array[Math.floor(Math.random() * array.length)];
 
-const seedDB = async()=>{
+const seedDB = async () => {
     await campground.deleteMany({});
-    for(let i=0; i<50; i++){
-        const random1000= Math.floor(Math.random()*1000);
-        const camp= new Campground({
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
+        const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: "https://source.unsplash.com/collection/483251",
+            description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+            price
         })
-    await camp.save();
+        await camp.save();
     }
 }
 
